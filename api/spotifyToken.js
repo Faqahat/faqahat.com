6d55e5015b9b0cc56550cca4e0cc3bad7509a268
Faqahat <Faqahat@gmx.com>
2019-11-26 01:33:07 +0500

    Added Now Secrets for Enviornment Variables

--- api/spotifyToken.js
+++ api/spotifyToken.js
@@ -1,21 +1,25 @@
 let axios = require('axios');
+
+const refreshToken = process.env.refreshtoken;
+const userauth = process.env.userauth;
 module.exports = (req, res) => {
+
     axios({
         url: "https://accounts.spotify.com/api/token",
         method: "post",
         params: {
             grant_type: "refresh_token",
-            refresh_token: 'AQDoJJPTp4Tpp1WUr-MzB9rI1p-3aHu3hIuT866A9mMYGtUoJB6GkYZZFqPWrqDAMueN9f5Hq_LTwxj5NgCW64q6rTP2OMyGK-zKXxTeIKmxrOVAHKIrLsyyuPBnzBHMQRJxAg'
+            refresh_token: refreshToken
         },
         headers: {
             "Accept": "application/json",
             'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization': 'Basic OWY2YTZlMzk2ZTE3NDc2MDg4MjhkMmNmMjdkODE5YTQ6YjBlOWU0ODM4MDA4NDQ3YjhhZmI4MmU3YjExODUzNTE=' // client id and secret from env
+          'Authorization': `Basic ${userauth}` // client id and secret from env
         },
 
     })
     .then((response) => {
-        console.log(response.data)
+        console.log(response.data + "Token = " + refreshToken)
         res.send(response.data.access_token);
     })
     .catch((error) => {
